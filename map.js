c2f555184de36668decb520d65be7164d06891a7
ligdiane <dli@goodrx.com>
2021-04-25 18:50:27 -0400

    saving project files before cleaning

--- map.js
+++ map.js
@@ -1,51 +1,84 @@
-var color = d3.scaleSequential(d3.interpolateGnBu).domain([0,120]);
+//reference: https://bl.ocks.org/d3noob/7030f35b72de721622b8
 
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
 
 d3.json("test3.json", function(error, nyc) {
-  if (error) throw error;
+    if (error) throw error;
 
-  var path = d3.geoPath()
-      .projection(d3.geoConicConformal()
-      .parallels([33, 45])
-      .rotate([96, -39])
-      .fitSize([width, height], nyc));
+    var color = d3.scaleSequential(d3.interpolateGnBu).domain([0, 120]);
 
-  svg.selectAll("path")
-      .data(nyc.features)
-      .enter().append("path")
-      .attr("d", path)
-      .attr("stroke-width",0.2)
-      .attr("stroke","white")
-      .attr('fill',function(d) { return color(Math.sqrt(d.properties.total_pop));})
+    var width = 800;
+    var height = 820;
 
-      .on("mouseenter", function(d) {
-        console.log(d);
 
-      d3.select(this)
-      .style("stroke-width", 1.5)
-      .style("stroke","black")
+    d3.select("#maptest").append("input")
+        .attr("type", "button")
+        .attr("value", "Toggle")
+        .attr("onclick", "togglePressed()");
 
-      d3.select("#neighborhoodPopover")
-      .transition()
-      .style("opacity", 1)
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", (d3.event.pageY) + "px")
-      .text(d.properties.ntaname + ", Total Population: " + d.properties.total_pop)
 
-    })
-    .on("mouseleave", function(d) { 
-      d3.select(this)
-      .style("stroke-width", 0)
-      .style("fill",function(d) { return color(Math.sqrt(d.properties.total_pop));})
+    var svg = d3.select("#maptest")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("background-color", '#E8F8F5');
 
-      d3.select("#cneighborhoodPopoverountyText")
-      .transition()
-      .style("opacity", 0);
 
-    });
 
-    console.log(nyc);
+    var g = svg.append("g");
+
+
+    var zoom = d3.zoom()
+        .scaleExtent([1, 2])
+        .translateExtent([
+            [-500, -300],
+            [1500, 1000]
+        ])
+        .on('zoom', () => {
+            g.attr('transform', d3.event.transform)
+        });
+
+    svg.call(zoom);
+
+
+    var path = d3.geoPath()
+        .projection(d3.geoConicConformal()
+            .parallels([33, 45])
+            .rotate([96, -39])
+            .fitSize([width, height], nyc));
+
+    g.selectAll("path")
+        .data(nyc.features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("stroke-width", 0.2)
+        .attr("stroke", "white")
+        .attr('fill', function(d) { return color(Math.sqrt(d.properties.total_pop)); })
+
+        .on("mouseenter", function(d) {
+            console.log(d);
+
+            d3.select("#neighborhoodPopover")
+                .transition()
+                .style("opacity", 1)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .text(d.properties.ntaname + ", Total Population: " + d.properties.total_pop)
+
+        })
+        .on("mouseleave", function(d) {
+            d3.select(this)
+                .style("stroke-width", 0)
+                .style("fill", function(d) { return color(Math.sqrt(d.properties.total_pop)); })
+
+            d3.select("#neighborhoodPopoverountyText")
+                .transition()
+                .style("opacity", 0);
+
+        });
 });
+
+function togglePressed() {
+    d3.select("#maptest").select("svg").transition().style("background-color", '#000000');
+    console.log("hello");
+
+};
